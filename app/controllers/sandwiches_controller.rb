c3ec64ccd0089b89488ab7441233e219fb06a210
Andreas Finger <webmaster@mediafinger.com>
2016-05-01 12:38:43 +0200

    Add SandwichesController #index and #show actions

---
+++ app/controllers/sandwiches_controller.rb
@@ -0,0 +1,11 @@
+class SandwichesController < ApplicationController
+  def index
+    sandwiches = Sandwich.all.limit(1000)
+    render json: sandwiches
+  end
+
+  def show
+    sandwich = Sandwich.find(params[:id])
+    render json: sandwich
+  end
+end
