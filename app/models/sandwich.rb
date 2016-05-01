444851361855844438996c95983ec4687806fa6e
Andreas Finger <webmaster@mediafinger.com>
2016-05-01 13:11:48 +0200

    Add validation to Sandwich name

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,4 +1,6 @@
 class Sandwich < ActiveRecord::Base
   has_many :sandwich_ingredients
   has_many :ingredients, through: :sandwich_ingredients
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 255 }
 end
