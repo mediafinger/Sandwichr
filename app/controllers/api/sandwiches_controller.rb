729b04feed78f313fb8700fe7648557b5835d9e8
Andreas Finger <webmaster@mediafinger.com>
2016-05-01 13:43:12 +0200

    Extract find_by and error message to before_action

--- app/controllers/api/sandwiches_controller.rb
+++ app/controllers/api/sandwiches_controller.rb
@@ -1,17 +1,13 @@
 class Api::SandwichesController < ApplicationController
+  before_action :ensure_sandwich, only: [:show, :update]
+
   def index
     sandwiches = Sandwich.all.limit(1000)
     render json: sandwiches
   end
 
   def show
-    sandwich = Sandwich.find_by(id: params[:id])
-
-    if sandwich
-      render json: sandwich, status: :ok
-    else
-      render json: { errors: "Sandwich with id #{params[:id]} not found" }, status: 404
-    end
+    render json: @sandwich, status: :ok
   end
 
   def create
@@ -25,16 +21,10 @@ class Api::SandwichesController < ApplicationController
   end
 
   def update
-    sandwich = Sandwich.find_by(id: params[:id])
-
-    if sandwich
-      if sandwich.update(sandwich_params)
-        render json: sandwich, status: :ok
-      else
-        render json: { errors: sandwich.errors.full_messages }, status: 422
-      end
+    if @sandwich.update(sandwich_params)
+      render json: @sandwich, status: :ok
     else
-      render json: { errors: "Sandwich with id #{params[:id]} not found" }, status: 404
+      render json: { errors: @sandwich.errors.full_messages }, status: 422
     end
   end
 
@@ -42,5 +32,10 @@ class Api::SandwichesController < ApplicationController
 
   def sandwich_params
     params.require(:sandwich).permit(:name, :bread_type)
+  end
+
+  def ensure_sandwich
+    @sandwich = Sandwich.find_by(id: params[:id])
+    render(json: { errors: "Sandwich with id #{params[:id]} not found" }, status: 404) unless @sandwich
   end
 end
