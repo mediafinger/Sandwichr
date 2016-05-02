96a686ae136387e90a7b6072505f53196d2fce53
Andreas Finger <webmaster@mediafinger.com>
2016-05-02 20:28:30 +0200

    Add .css to Rails how-to

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :sandwiches, only: [:index, :show, :create, :update, :destroy]
   end
 
+  get "home" => "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
